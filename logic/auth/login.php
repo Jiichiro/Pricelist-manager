d8be6c59397cfc9939d3d053cb6611e5b349b42f
unknown <rvlinadwi@gmail.com>
2025-09-27 15:41:38 +0700

    deskripsi

--- logic/auth/login.php
+++ logic/auth/login.php
@@ -1,1 +1,14 @@
-<!-- code -->
+<?php include __DIR__ . "/../components/header.php"; ?>
+
+<h2>Login Page</h2>
+<form method="post" action="">
+    <label>Username:</label>
+    <input type="text" name="username"><br><br>
+
+    <label>Password:</label>
+    <input type="password" name="password"><br><br>
+
+    <button type="submit">Login</button>
+</form>
+
+<?php include __DIR__ . "/../components/footer.php"; ?>
