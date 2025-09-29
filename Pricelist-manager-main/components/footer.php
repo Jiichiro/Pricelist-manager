5f4d81cd73fdf6217e25ad358c196a80d5a017d4
Jiichiro <cokmesoh@gmail.com>
2025-09-29 08:17:03 +0700

    fixing folder format

--- Pricelist-manager-main/components/footer.php
+++
@@ -1,7 +0,0 @@
-</main>
-<footer>
-    <p>&copy; 2024 Price List Manager</p>
-</footer>
-</body>
-
-</html>
--- Pricelist-manager-main/pages/seller.php
+++
@@ -1,4 +0,0 @@
-<?php include("../components/header.php"); ?>
-<h1>Seller dashboard</h1>
-<p>Welcome to your dashboard, Seller!</p>
-<?php include("../components/footer.php"); ?>
